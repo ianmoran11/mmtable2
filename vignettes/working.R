049f8de2c5e4003e36e41ed6873f2931b5436c81
Ian Moran <ian.moran11@gmail.com>
2021-04-11 21:04:44 +1000

    Adding mit license

--- vignettes/working.R
+++ vignettes/working.R
@@ -5,8 +5,11 @@ library(gt)
 library(tidyverse)
 devtools::load_all()
 # library(mmtable2)
+options(warn=-1)
 
 list.files("methods/",full.names = T) %>% map(source)
+
+student_df
 
 table1 <-
   student_df %>%
@@ -32,8 +35,32 @@ table2 <-
   header_left(class) +
   header_left_top(subject)
 
-table1             +  table_title("Single Table")
-table1 / table1.1  +  table_title("Table Quotient")
-table1 * table1.1  +  table_title("Table Product")
-table1 + table2    +  table_title("Table Sum")
+# table1 <-
+student_df %>%
+  mmtable(table_data = value)  +
+    header_top(student) +
+    header_top_left(grade) +
+    header_left(class) +
+    header_left_top(subject)
+
+table1            + table_title("Single Table")
+table1 / table1.1 + table_title("Table Quotient")
+table1 * table1.1 + table_title("Table Product")
+table1 + table2   + table_title("Table Sum")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
