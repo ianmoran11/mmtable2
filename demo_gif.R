08d1d324c68d67cc86244ab6be8b4805188fd12a
Ian Moran <ian.moran11@gmail.com>
2021-09-18 09:44:52 +1000

    pre-gif

--- demo_gif.R
+++ demo_gif.R
@@ -1,23 +1,4 @@
 
-gm_df <- gapminder_mm %>% filter(var != "Life expectancy")
-
-style_list <- list(
-    cell_borders(sides = "top",color = "grey")
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  gm_df %>%
   mmtable(cells = value) +
   header_top(year) +
@@ -28,3 +9,35 @@ style_list <- list(
       continent, scope = "table",
       style = style_list
       )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gm_df <- gapminder_mm %>% filter(var != "Life expectancy")
+
+style_list <- list(
+    cell_borders(sides = "top",color = "grey")
+    )
