7d8c2ac20a0cf18713db16b9285992eef1886dcf
Ian <ian.moran11@gmail.com>
2021-09-18 01:43:50 +0000

    updated site

---
+++ tenmp.cpp
@@ -0,0 +1,32 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+ hello   struct {
+    int v = 65 ;
+    return x * 2* v;
+  };
+
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+
+*/
